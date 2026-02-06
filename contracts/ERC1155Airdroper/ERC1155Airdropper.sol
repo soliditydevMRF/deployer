775c2f451cbcdacc5c1da4648513061e119621e0
Ruslan <rusakdemon@gmail.com>
2026-02-06 15:55:24 +0500

    refactor: editing utility contracts

--- contracts/ERC1155Airdroper/ERC1155Airdropper.sol
+++ contracts/ERC1155Airdroper/ERC1155Airdropper.sol
@@ -1,61 +1,70 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
+import "../UtilityContract/AbstractUtilityContract.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "../IUtilityContract.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC1155Airdroper is IUtilityContract, Ownable {
-    constructor() Ownable(msg.sender) {}
+contract ERC1155Airdroper is AbstractUtilityContract, Ownable {
+    constructor() payable Ownable(msg.sender) {}
 
     uint256 public constant MAX_AIRDROP_BATCH_SIZE = 10;
 
     IERC1155 public token;
     address public treasury;
 
-    error AlareadyInitialized();
-    error ResieversLengthMismatch();
-    error NeedToApproveTokens();
-    error TransferFailed();
+    error AlreadyInitialized();
+    error ReceiversLengthMismatch();
     error AmountsLengthMismatch();
     error BatchSizeExceeded();
+    error NeedToApproveTokens();
 
     modifier notInitialized() {
-        require(!initialized, AlareadyInitialized());
+        require(!initialized, AlreadyInitialized());
         _;
     }
 
     bool private initialized;
 
-    function initialize(bytes memory _initData) external notInitialized returns (bool) {
-        (address _token, address _treasury, address _owner) = abi.decode(_initData, (address, address, address));
-
-        token = IERC1155(_token);
-        treasury = _treasury;
-        Ownable.transferOwnership(_owner);
-
-        initialized = true;
-
-        return true;
-    }
-
-    function airdrop(address[] calldata _receivers, uint256[] calldata _amounts, uint256[] calldata tokenIds)
+    function airdrop(address[] calldata receivers, uint256[] calldata amounts, uint256[] calldata tokenIds)
         external
         onlyOwner
     {
         require(tokenIds.length <= MAX_AIRDROP_BATCH_SIZE, BatchSizeExceeded());
-        require(_receivers.length == tokenIds.length, ResieversLengthMismatch());
-        require(_amounts.length == tokenIds.length, AmountsLengthMismatch());
+        require(receivers.length == tokenIds.length, ReceiversLengthMismatch());
+        require(amounts.length == tokenIds.length, AmountsLengthMismatch());
         require(token.isApprovedForAll(treasury, address(this)), NeedToApproveTokens());
 
-        for (uint256 i = 0; i < _amounts.length; i++) {
-            token.safeTransferFrom(treasury, _receivers[i], tokenIds[i], _amounts[i], "");
+        address treasuryAddress = treasury;
+
+        for (uint256 i = 0; i < amounts.length;) {
+            token.safeTransferFrom(treasuryAddress, receivers[i], tokenIds[i], amounts[i], "");
+            unchecked {
+                ++i;
+            }
         }
     }
 
-    function getInitData(address _token, address _treasury, address _owner) external pure returns (bytes memory) {
-        return abi.encode(_token, _treasury, _owner);
+    function initialize(bytes memory _initData) external override notInitialized returns (bool) {
+        (address _deployManager, address _token, address _treasury, address _owner) =
+            abi.decode(_initData, (address, address, address, address));
+
+        setDeployManager(_deployManager);
+
+        token = IERC1155(_token);
+        treasury = _treasury;
+
+        Ownable.transferOwnership(_owner);
+
+        initialized = true;
+        return true;
+    }
+
+    function getInitData(address _deployManager, address _token, address _treasury, address _owner)
+        external
+        pure
+        returns (bytes memory)
+    {
+        return abi.encode(_deployManager, _token, _treasury, _owner);
     }
 }
-// ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"]
-
--- contracts/ERC20Airdroper/ERC20Airdropper.sol
+++ contracts/ERC20Airdroper/ERC20Airdropper.sol
@@ -1,59 +1,75 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../UtilityContract/AbstractUtilityContract.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20Airdropper is AbstractUtilityContract, Ownable {
-    constructor() Ownable(msg.sender) {}
+/// @title ERC20Airdroper - Airdrop utility contract for ERC20 tokens
+/// @author Solidity University
+/// @notice This contract allows the owner to airdrop ERC20 tokens to multiple addresses.
+/// @dev Inherits from AbstractUtilityContract and Ownable
+contract ERC20Airdroper is AbstractUtilityContract, Ownable {
+    constructor() payable Ownable(msg.sender) {}
+
+    uint256 public constant MAX_AIRDROP_BATCH_SIZE = 300;
 
     IERC20 public token;
-    uint256 public amount; // с учетом decimals
+    uint256 public amount;
     address public treasury;
 
-    error AlareadyInitialized();
+    error AlreadyInitialized();
     error ArraysLengthMismatch();
-    error NotEnoughtApprovedTokens();
+    error NotEnoughApprovedTokens();
     error TransferFailed();
+    error BatchSizeExceeded();
 
     modifier notInitialized() {
-        require(!initialized, AlareadyInitialized());
+        require(!initialized, AlreadyInitialized());
         _;
     }
-    //деплоер первый аккаунт
+
     bool private initialized;
 
+    function airdrop(address[] calldata receivers, uint256[] calldata amounts) external onlyOwner {
+        require(receivers.length <= MAX_AIRDROP_BATCH_SIZE, BatchSizeExceeded());
+        require(receivers.length == amounts.length, ArraysLengthMismatch());
+        require(token.allowance(treasury, address(this)) >= amount, NotEnoughApprovedTokens());
+
+        address treasuryAddress = treasury;
+
+        for (uint256 i = 0; i < receivers.length;) {
+            require(token.transferFrom(treasuryAddress, receivers[i], amounts[i]), TransferFailed());
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
     function initialize(bytes memory _initData) external override notInitialized returns (bool) {
-        (address _token, uint256 _amount, address _treasury, address _owner) =
-            abi.decode(_initData, (address, uint256, address, address));
+        (address _deployManager, address _token, uint256 _amount, address _treasury, address _owner) =
+            abi.decode(_initData, (address, address, uint256, address, address));
+
+        setDeployManager(_deployManager);
 
         token = IERC20(_token);
         amount = _amount;
         treasury = _treasury;
+
         Ownable.transferOwnership(_owner);
 
         initialized = true;
-
         return true;
     }
 
-    function airdrop(address[] calldata _receivers, uint256[] calldata _amounts) external onlyOwner {
-        require(_receivers.length == _amounts.length, ArraysLengthMismatch());
-        require(token.allowance(treasury, address(this)) >= amount, NotEnoughtApprovedTokens());
-
-        for (uint256 i = 0; i < _receivers.length; i++) {
-            require(token.transferFrom(treasury, _receivers[i], _amounts[i]), TransferFailed());
-        }
-    }
-
-    function getInitData(address _token, uint256 _amount, address _treasury, address _owner)
+    function getInitData(address _deployManager, address _token, uint256 _amount, address _treasury, address _owner)
         external
         pure
         returns (bytes memory)
     {
-        return abi.encode(_token, _amount, _treasury, _owner);
+        return abi.encode(_deployManager, _token, _amount, _treasury, _owner);
     }
 }
-// ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"]
 
+//["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB","0x617F2E2fD72FD9D5503197092aC168c91465E7f2"]
+//[2500000000000000000000,3100000000000000000000,1900000000000000000000,2500000000000000000000]
